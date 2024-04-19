5b77126b6dc879b136e8cd4bf1f5ec7b70496028
kazumakano <kazumakano@outlook.jp>
2024-04-19 13:20:05 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 echo alias cdcd=\'cd /mnt/bigdata/00_students/kazuma_nis/\' >> ~/.bashrc
 
-echo alias ga=\'git add\' >> ~/.bashrc
-echo alias gc=\'git commit \-m\' >> ~/.bashrc
-echo alias gs=\'git switch\' >> ~/.bashrc
-
 echo alias ta=\'tmux attach-session \-t\' >> ~/.bashrc
 echo alias tl=\'tmux list-session\' >> ~/.bashrc
