ae82ebc3d893d8762fd4f18a6618649fa8e5c250
kazumakano <kazumakano@outlook.jp>
2024-04-19 13:22:25 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ echo alias cdcd=\'cd /mnt/bigdata/00_students/kazuma_nis/\' >> ~/.bashrc
 
 echo alias ta=\'tmux attach-session \-t\' >> ~/.bashrc
 echo alias tl=\'tmux list-session\' >> ~/.bashrc
+echo alias tn=\'tmux new-session \-t\' >> ~/.bashrc
